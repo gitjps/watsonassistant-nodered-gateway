4e1212f00216728190a435ba6f42e7a79331caa2
Peter Schleinitz <jens-peter.schleinitz@de.ibm.com>
2019-04-29 13:52:49 +0200

    Create noderedgateway.js

---
+++ noderedgateway.js
@@ -0,0 +1,14 @@
+var rp = require('request-promise');
+function main(params) {
+    var uri = params.target_hostname + params.target_route + params.query_params;
+    return new Promise(function (resolve, reject) {
+       rp(uri)
+       .then(function (parsedBody) {
+           obj = JSON.parse(parsedBody);
+           resolve({ "gw_result": obj });
+       })
+       .catch(function (err) {
+           resolve({ message: 'failed!!', error: err.toString() });
+       });
+    });
+}
